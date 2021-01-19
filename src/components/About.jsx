3929ed3929c6e9e1e4118ce013639f18c7dafb3e
Mclara1 <mclara1@ua.pt>
2021-01-19 19:50:56 +0000

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,30 +5,12 @@ function About() {
       <div id="about">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-md-6"> <img src="img/about.jpg" className="img-responsive" alt=""/> </div>
+          <div className="col-xs-12 col-md-6"> <img src="img/travel.jpg" className="img-responsive" alt=""/> </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
               <h2>About Us</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-              <h3>Why Choose Us?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    <li>Lorem ipsum dolor</li>
-                    <li>Lorem ipsum dolor</li>
-                    <li>Lorem ipsum dolor</li>
-                    <li>Lorem ipsum dolor</li>
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    <li>Lorem ipsum dolor</li>
-                    <li>Lorem ipsum dolor</li>
-                    <li>Lorem ipsum dolor</li>
-                    <li>Lorem ipsum dolor</li>
-                  </ul>
-                </div>
-              </div>
+              <p>When we started, we set out to do things differently. We wanted to show all flight options in one place, creating a simple alternative to the confusing sites that make travel feel like hard work. Now we are dedicated to making travel as simple as possible, we help each and every one of them find the best options across flights and hotels to book the perfect trip.</p>
+              <p>But we’re not stopping there.</p>
             </div>
           </div>
         </div>
