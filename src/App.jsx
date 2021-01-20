a01416554ff24527c3cadd4090c49766677f060e
hmtrilz <beatrizso1@hotmail.com>
2021-01-20 11:55:21 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, { useState } from "react";
 import Navigation from "./pages/Navigation";
 import Header from "./pages/Header";
@@ -27,16 +28,33 @@ import FlightResults from './components/FlightResults';
 import HotelResults from './components/HotelResults';
 import DestinationsItem from './components/DestinationsItem';
 import BingApi from './components/BingApi';
+=======
+import React, { useState } from 'react'
+import Navigation from './pages/Navigation';
+import Header from './pages/Header';
+import About from './pages/About';
+import How from './pages//How';
+import Destinations from './pages/Destinations';
+import Results from './pages/Results';
+import Contact from './pages/Contact';
+import DestinationsItem from './pages/DestinationsItem';
+import BingApi from './pages/BingApi';
+
+>>>>>>> parent of 160ec2e... pastas e afins
 
 function App () {
   const [apiData, setApiData] = useState (null);
   const [hotelData, setHotelData] = useState (null);
   const [country, setCountry] = useState ();
+<<<<<<< HEAD
 >>>>>>> parent of 8d9120d... router
+=======
+>>>>>>> parent of 160ec2e... pastas e afins
 
   return (
     <div>
       <Navigation />
+<<<<<<< HEAD
 <<<<<<< HEAD
       <Header setApiData={setApiData} setHotelData={setHotelData} />
 =======
@@ -44,14 +62,19 @@ function App () {
 >>>>>>> parent of 8d9120d... router
       <FlightResults apiData={apiData} />
       <HotelResults hotelData={hotelData} />
+=======
+      <Header setApiData={setApiData} setHotelData={setHotelData}/>
+>>>>>>> parent of 160ec2e... pastas e afins
       <About />
       <How />
       <Destinations setCountry={setCountry} />
-      <DestinationsItem country={country} />
+      <DestinationsItem country={country}/>
       <BingApi country={country} />
       <Contact />
+      <Results />
     </div>
-  );
+  )
+  
 }
 
 export default App;
